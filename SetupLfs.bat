b51ff529c79a9ae7e0c5ed2f6372e23cf53dfb04
VIDEOHAZE <ken@cosmiclab.jp>
2022-05-31 12:53:04 +0900

    Fixed LFS path.

--- SetupLfs.bat
+++ SetupLfs.bat
@@ -1,4 +1,4 @@
-set dropboxPath="%CosmicProjects%/.."
+set dropboxPath="%CosmicProjects%"
 set projectName=220629_FRDC_Yoyogi
 
 call git config --unset-all lfs.customtransfer.lfs-folder.path
@@ -6,7 +6,7 @@ call git config --unset-all lfs.customtransfer.lfs-folder.args
 call git config --unset-all lfs.standalonetransferagent
 
 call git config --add lfs.customtransfer.lfs-folder.path lfs-folderstore
-call git config --add lfs.customtransfer.lfs-folder.args "%dropboxPath%/LFS_for_ALL/%projectName%/LFS"
+call git config --add lfs.customtransfer.lfs-folder.args "%dropboxPath%/%projectName%/LFS"
 call git config --add lfs.standalonetransferagent lfs-folder
 
 call git reset --hard master
